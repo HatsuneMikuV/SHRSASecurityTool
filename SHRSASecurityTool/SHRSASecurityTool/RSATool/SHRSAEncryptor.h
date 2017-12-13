8c7f0fa66970fd9e799dae9f6bb4ca75213c3395
Icarus <15188588180@163.com>
2017-12-13 12:50:03 +0800

    RSA  加密方法

---
+++ SHRSASecurityTool/SHRSASecurityTool/RSATool/SHRSAEncryptor.h
@@ -0,0 +1,46 @@
+//
+//  SHRSAEncryptor.h
+//  SHRSASecurityTool
+//
+//  Created by angle on 2017/12/13.
+//  Copyright © 2017年 angle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHRSAEncryptor : NSObject
+
+/**
+ *  加密方法
+ *
+ *  @param str   需要加密的字符串
+ *  @param path  '.der'格式的公钥文件路径
+ */
++ (NSString *)encryptString:(NSString *)str publicKeyWithContentsOfFile:(NSString *)path;
+
+/**
+ *  解密方法
+ *
+ *  @param str       需要解密的字符串
+ *  @param path      '.p12'格式的私钥文件路径
+ *  @param password  私钥文件密码
+ */
++ (NSString *)decryptString:(NSString *)str privateKeyWithContentsOfFile:(NSString *)path password:(NSString *)password;
+
+/**
+ *  加密方法
+ *
+ *  @param str    需要加密的字符串
+ *  @param pubKey 公钥字符串
+ */
++ (NSString *)encryptString:(NSString *)str publicKey:(NSString *)pubKey;
+
+/**
+ *  解密方法
+ *
+ *  @param str     需要解密的字符串
+ *  @param privKey 私钥字符串
+ */
++ (NSString *)decryptString:(NSString *)str privateKey:(NSString *)privKey;
+
+@end
---
+++ SHRSASecurityTool/SHRSASecurityTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHRSASecurityTool
+//
+//  Created by angle on 2017/12/13.
+//  Copyright © 2017年 angle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
